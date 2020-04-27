baf1f7d89eea3e9d618679b8de914fa28965ba83
Davide Mastruzzo <mastruzzodavide@gmail.com>
2020-04-27 14:39:52 +0200

    fix hoodie category

--- src/main/resources/h2/imports/03_item.sql
+++ src/main/resources/h2/imports/03_item.sql
@@ -4,5 +4,5 @@ INSERT INTO item (id, name, price, category_id) VALUES
 (3, 'Supreme Tv Tee', '38 Fr', 3),
 (4, 'Supreme S Logo Cap', '50 Fr', 4),
 (5, 'Calvin Klein', '30 Fr', 5),
-(6, 'Vetements Metal Hoodie', '500 Fr', 5)
+(6, 'Vetements Metal Hoodie', '500 Fr', 6)
 ;
